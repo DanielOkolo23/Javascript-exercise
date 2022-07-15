bacae4b455380a6f096f826156d593d9a8355c3a
Summertime <32138488+zaddy1@users.noreply.github.com>
2022-07-15 10:56:43 +0000

    attempt challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,7 +14,7 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-   let sum =0;
+   let sum = 0;
   for (let i =0; i < arrayOfNumbers.length; i++) {
     sum +=arrayOfNumbers[i]
   }
